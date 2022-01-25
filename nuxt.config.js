fa0f401823547fd5577ad623057a67bd1bc85144
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2022-01-25 13:19:48 -0400

    Initialize Nuxt Bridge

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
 
-export default {
+import { defineNuxtConfig } from '@nuxt/bridge'
+
+export default defineNuxtConfig({
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - DeepNotes-Pages',
@@ -76,4 +78,4 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
-}
+})
