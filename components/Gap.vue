0cd10a9c45ab4bdaf68751e7a9e6ca4262135814
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-01-28 07:47:15 -0400

    Improve Gap component

--- components/Gap.vue
+++ components/Gap.vue
@@ -1,12 +1,21 @@
 <template>
 
-  <div :style="$attrs"></div>
+  <div :style="{
+    ...$attrs,
+    display: inlineBlock ? 'inline-block' : null,
+  }"
+  style="flex: none">
+  </div>
   
 </template>
 
 <script>
 export default {
   inheritAttrs: false,
+
+  props: {
+    inlineBlock: { type: Boolean },
+  },
 }
 </script>
 
