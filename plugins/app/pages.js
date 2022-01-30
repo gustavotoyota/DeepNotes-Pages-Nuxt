512ef4da20baa959254beeaf3ccb1d52027999e0
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-01-30 10:25:58 -0400

    Adjust store structures

--- plugins/app/pages.js
+++ plugins/app/pages.js
@@ -12,21 +12,26 @@ export default pages
 
 
 pages.create = (name) => {
-  const store = syncedStore({ page: {} })
-
-
-
-  
-  const page = store.page
+  const page = {}
 
   page.id = uuidv4()
 
-  page.name = name
+  const store = new syncedStore({ collab: {} })
+  page.collab = store.collab
+  $merge(page.collab, {
+    name: 'Main page',
+
+    lockPos: false,
+    lockZoom: false,
+
+    blockIds: [],
+    arrowIds: [],
+  })
 
   page.elems = {
     blocks: [],
     arrows: [],
-    
+
     regionId: null,
     selected: {},
     activeId: null,
@@ -36,25 +41,22 @@ pages.create = (name) => {
   page.camera = {
     pos: { x: 0, y: 0 },
     zoom: 1,
-
-    lockPos: false,
-    lockZoom: false,
   }
 
-  if (process.client) {
-    const doc = getYjsValue(store);
 
-    const name = `page-${store.page.id}`
+
+  if (process.client) {
+    const name = `page-${page.id}`
+    const doc = getYjsValue(store)
 
     new WebsocketProvider(
       $context.isDev ? "ws://localhost:1234" : "wss://yjs-server.deepnotes.app/",
       name, doc)
-
     new IndexeddbPersistence(name, doc)
   }
 
 
-  
+
 
   $state.project.pages.list.push(page)
 
