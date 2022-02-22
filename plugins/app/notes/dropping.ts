4485d191f2419788a89d72bbe471d7ac93a17978
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-02-22 16:13:43 -0400

    Remove dangerous transaction

--- plugins/app/notes/dropping.ts
+++ plugins/app/notes/dropping.ts
@@ -26,31 +26,29 @@ class AppDropping {
 
 
   perform(event: PointerEvent, regionNote: Note, dropIndex: number) {
-    this.ctx.$app.collab.doc.transact(() => {
-      this.ctx.$app.dragging.finish(event)
+    this.ctx.$app.dragging.finish(event)
+    
+
+
+
+    const selectedNotes = this.ctx.$app.selection.notes.splice(0).reverse()
+
+    for (const selectedNote of selectedNotes) {
+      selectedNote.removeFromRegion()
+
+      regionNote.collab.childIds.splice(dropIndex, 0, selectedNote.id)
+      selectedNote.parentId = regionNote.id
+    }
+
+    this.ctx.$app.activeRegion.id = regionNote.id
+
+
+
+
+    Vue.nextTick(() => {
+      const lastSelectedNote = this.ctx.$app.selection.notes.at(-1) as Note
       
-
-
-
-      const selectedNotes = this.ctx.$app.selection.notes.splice(0).reverse()
-
-      for (const selectedNote of selectedNotes) {
-        selectedNote.removeFromRegion()
-
-        regionNote.collab.childIds.splice(dropIndex, 0, selectedNote.id)
-        selectedNote.parentId = regionNote.id
-      }
-
-      this.ctx.$app.activeRegion.id = regionNote.id
-
-
-
-
-      Vue.nextTick(() => {
-        const lastSelectedNote = this.ctx.$app.selection.notes.at(-1) as Note
-        
-        lastSelectedNote.scrollIntoView()
-      })
+      lastSelectedNote.scrollIntoView()
     })
   }
 }
