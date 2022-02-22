5aca0e0d9e31cc167b707978efd0651b1e273899
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-02-22 14:13:22 -0400

    Implement page properties

--- components/RightSidebar/PageProperties.vue
+++ components/RightSidebar/PageProperties.vue
@@ -1,8 +1,10 @@
 <template>
 
-  <div>
+  <div style="height: 100%;
+  display: flex;
+  flex-direction: column">
 
-    <v-toolbar>
+    <v-toolbar style="flex: none">
       <v-list-item-icon>
         <v-icon>mdi-chart-box</v-icon>
       </v-list-item-icon>
@@ -11,6 +13,39 @@
         Page Properties
       </v-toolbar-title>
     </v-toolbar>
+
+
+
+    
+    <div style="flex: 1;
+    overflow-y: auto"
+    class="pb-4">
+      
+      <div class="mx-5 mt-4">
+        <div class="body-2 grey--text text--lighten-1">
+          Name:
+        </div>
+
+        <v-text-field dense solo hide-details style="clear: right"
+        class="mt-1 body-2" v-model="$app.page.collab.name">
+        </v-text-field>
+      </div>
+
+      <v-divider class="mt-4"/>
+        
+      <div class="mx-5 mt-4">
+        <v-checkbox hide-details label="Lock position"
+        style="flex: 1; margin-top: 0; padding-top: 0"
+        v-model="$app.camera.lockPos"/>
+
+        <Gap height="10px"/>
+
+        <v-checkbox hide-details label="Lock zoom"
+        style="flex: 1; margin-top: 0; padding-top: 0"
+        v-model="$app.camera.lockZoom"/>
+      </div>
+
+    </div>
 
   </div>
   
