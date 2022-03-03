76a9b88a9dea3a2d81d8f41d8ecfac49f9d3a22b
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-03-03 13:25:37 -0400

    Implement shift-selection

--- plugins/app/page/selection/click-selection.ts
+++ plugins/app/page/selection/click-selection.ts
@@ -1,6 +1,7 @@
 import { Context } from "@nuxt/types"
 import { AppPage } from "../page"
 import { Elem } from "../elems/elems"
+import { Note } from "../notes/notes"
 
 
 
@@ -26,6 +27,22 @@ class AppClickSelection {
 
 
   perform(elem: Elem, event: PointerEvent) {
+    // Container shift-selection
+  
+    if (elem.parentId != null
+    && event.shiftKey && this.page.activeElem.exists) {
+      const fromIndex = (this.page.activeElem.get as Note).index
+      const toIndex = (elem as Note).index
+  
+      const step = Math.sign(toIndex - fromIndex)
+  
+      for (let i = fromIndex; i !== toIndex; i += step)
+        this.page.selection.add(this.page.activeRegion.notes[i])
+    }
+
+
+
+
     // Clear selection if not holding Ctrl or Shift
     // And the clicked element is not selected
 
