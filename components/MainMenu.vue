f47f07e12fbd93bc3ccf7ae6f211d31a32bba405
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-03-07 15:17:48 -0400

    Create user icon on top-right to avoid accidentally clicking logout

--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -28,21 +28,35 @@
     </v-app-bar-title>
 
     <v-spacer/>
+    
+    <v-menu bottom offset-y
+    :min-width="140">
 
-    <v-btn depressed
-    :href="$ctx.isDev ? 'http://192.168.1.3:60379/' : 'https://deepnotes.app/'">
-      Home
-    </v-btn>
+      <template v-slot:activator="{ on, attrs }">
 
-    <v-btn depressed
-    :href="$ctx.isDev ? 'http://192.168.1.3:60379/account' : 'https://deepnotes.app/account'">
-      Account
-    </v-btn>
+        <v-btn icon x-large
+        :width="36" :height="36"
+        v-bind="attrs" v-on="on"
+        style="margin-right: 0">
+          <v-icon>mdi-account-circle</v-icon>
+        </v-btn>
 
-    <v-btn depressed
-    @click="$auth.logout()">
-      Logout
-    </v-btn>
+      </template>
+
+      <v-list dense>
+
+        <v-list-item
+        :href="$ctx.isDev ? 'http://192.168.1.3:60379/account' : 'https://deepnotes.app/account'">
+          <v-list-item-title>Account</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item @click="$auth.logout()">
+          <v-list-item-title>Logout</v-list-item-title>
+        </v-list-item>
+
+      </v-list>
+
+    </v-menu>
     
   </v-app-bar>
   
@@ -51,6 +65,23 @@
 <script setup lang="ts">
 </script>
 
-<style>
+<style scoped>
+.v-app-bar /deep/ .v-toolbar__content {
+  padding-right: 8px;
+}
 
+
+
+
+.v-menu__content {
+  left: auto !important;
+  right: 5px;
+}
+
+
+
+
+.v-list {
+  padding: 0 !important;
+}
 </style>
