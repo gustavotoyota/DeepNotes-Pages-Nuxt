cce2ef6328c5547b1061b55b90a76109a93b9f47
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-03-10 16:24:22 -0400

    Change quill-editor.ts to quill.ts

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/external/highlight', mode: 'client' },
-    { src: '~/plugins/external/quill-editor', mode: 'client' },
+    { src: '~/plugins/external/quill', mode: 'client' },
     { src: '~/plugins/external/syncedstore' },
     
     { src: '~/plugins/static/static' },
