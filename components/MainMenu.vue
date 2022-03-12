4767a358f70f55563d9ccd025821537b3341e275
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-03-12 08:31:48 -0400

    Improve main menu popup buttons

--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -19,69 +19,38 @@
       </v-app-bar-title>
     </a>
 
-    <Gap width="16px" inline-block/>
-
-    <v-spacer/>
+    <Gap width="16px"/>
 
     <v-app-bar-title>
       {{ $app.page.data.name }}
     </v-app-bar-title>
 
     <v-spacer/>
+
+    <v-btn icon large
+    :width="32" :height="32">
+      <v-icon>mdi-cog</v-icon>
+    </v-btn>
+
+    <Gap width="8px"/>
     
-    <v-menu bottom offset-y
-    :min-width="140">
-
-      <template v-slot:activator="{ on, attrs }">
-
-        <v-btn icon x-large
-        :width="36" :height="36"
-        v-bind="attrs" v-on="on"
-        style="margin-right: 0">
-          <v-icon>mdi-account-circle</v-icon>
-        </v-btn>
-
-      </template>
-
-      <v-list dense>
-
-        <v-list-item
-        :href="$ctx.isDev ? 'http://192.168.1.3:60379/account' : 'https://deepnotes.app/account'">
-          <v-list-item-title>Account</v-list-item-title>
-        </v-list-item>
-
-        <v-list-item @click="$auth.logout()">
-          <v-list-item-title>Logout</v-list-item-title>
-        </v-list-item>
-
-      </v-list>
-
-    </v-menu>
+    <MainMenuPopup/>
     
   </v-app-bar>
   
 </template>
 
+
+
+
 <script setup lang="ts">
 </script>
+
+
+
 
 <style scoped>
 .v-app-bar /deep/ .v-toolbar__content {
   padding-right: 8px;
-}
-
-
-
-
-.v-menu__content {
-  left: auto !important;
-  right: 5px;
-}
-
-
-
-
-.v-list {
-  padding: 0 !important;
 }
 </style>
---
+++ components/MainMenu/MainMenuPopup.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <v-menu bottom offset-y
+  :min-width="150">
+
+    <template v-slot:activator="{ on, attrs }">
+
+      <v-btn icon x-large
+      :width="32" :height="32"
+      v-bind="attrs" v-on="on"
+      style="margin-right: 0">
+        <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+
+    </template>
+
+    <v-list dense>
+
+      <v-list-item
+      :href="$ctx.isDev ? 'http://192.168.1.3:60379/account' : 'https://deepnotes.app/account'">
+        <v-list-item-icon>
+          <v-icon>mdi-account</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Account</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item @click="$auth.logout()">
+        <v-list-item-icon>
+          <v-icon>mdi-logout</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Logout</v-list-item-title>
+      </v-list-item>
+
+    </v-list>
+
+  </v-menu>
+  
+</template>
+
+
+
+
+<style scoped>
+.v-menu__content {
+  left: auto !important;
+  right: 5px;
+}
+
+
+
+
+.v-list {
+  padding: 0 !important;
+}
+</style>
