34bbb4575ffff68ab3e88f7e519fdf6d5d1d518c
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-03-17 11:18:07 -0400

    Implement page sidebar minibuttons

--- components/RightSidebar/PageProperties.vue
+++ components/RightSidebar/PageProperties.vue
@@ -1,6 +1,52 @@
 <template>
 
-  <div v-if="!$app.project.collapsedRightSidebar">
+  <v-list v-if="$app.project.collapsedRightSidebar" dense>
+
+    <v-tooltip left>
+
+      <template v-slot:activator="{ on }">
+
+        <v-list-item v-on="on"
+        :input-value="$app.page.camera.lockPos"
+        @click="$app.page.camera.lockPos = !$app.page.camera.lockPos">
+          <v-list-item-icon>
+            <v-icon>mdi-axis-arrow-lock</v-icon>
+          </v-list-item-icon>
+        </v-list-item>
+
+      </template>
+
+      <span>Lock position</span>
+
+    </v-tooltip>
+
+
+
+
+    <v-tooltip left>
+
+      <template v-slot:activator="{ on }">
+
+        <v-list-item v-on="on"
+        :input-value="$app.page.camera.lockZoom"
+        @click="$app.page.camera.lockZoom = !$app.page.camera.lockZoom">
+          <v-list-item-icon>
+            <v-icon>mdi-arrow-vertical-lock</v-icon>
+          </v-list-item-icon>
+        </v-list-item>
+
+      </template>
+
+      <span>Lock zoom</span>
+
+    </v-tooltip>
+
+  </v-list>
+
+
+
+
+  <div v-else>
 
     <div class="mx-5 mt-4">
       <div class="body-2 grey--text text--lighten-1">
