3f0ee278447344f3fc978abc3bd2943c7f78b28a
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-03-18 14:54:42 -0400

    Allow page name wrapping in left sidebar

--- components/LeftSidebar.vue
+++ components/LeftSidebar.vue
@@ -6,31 +6,33 @@
   permanent
   touchless
   stateless
-  :mini-variant.sync="$app.project.collapsedLeftSidebar"
-  width="300">
+  :width="$app.project.collapsedLeftSidebar ? 56 : 300">
 
-    <v-toolbar dense>
+    <div style="height: 100%;
+    width: 300px;
+    display: flex;
+    flex-direction: column">
 
-      <v-icon style="position: relative; top: 1px">mdi-animation</v-icon>
+      <v-toolbar dense style="flex: none">
 
-
-
-
-      <v-toolbar-title v-if="!$app.project.collapsedLeftSidebar"
-      style="margin-left: 23px;
-      position: relative; top: 1px">
-        Path
-      </v-toolbar-title>
+        <v-icon style="position: relative; top: 1px">mdi-animation</v-icon>
 
 
 
 
-      <template v-if="!$app.project.collapsedLeftSidebar">
+        <v-toolbar-title style="margin-left: 23px;
+        position: relative; top: 1px">
+          Path
+        </v-toolbar-title>
+
+
+
+
         <v-spacer/>
 
 
 
-        
+          
         <v-btn icon :width="36" :height="36"
         :disabled="$app.project.pageIndex <= 0"
         @click="$app.project.navigatePath(-1)">
@@ -47,37 +49,43 @@
 
 
         <Gap width="0"/>
-      </template>
 
-    </v-toolbar>
+      </v-toolbar>
 
-    <v-list dense>
-      
-      <v-tooltip right
-      :disabled="!$app.project.collapsedLeftSidebar"
-      v-for="pageRef in $app.page.project.pathPages" :key="pageRef.id">
 
-        <template v-slot:activator="{ on }">
 
-          <v-list-item v-on="on"
-          :input-value="pageRef.id == $app.page.id"
-          @click="$app.project.navigateTo(pageRef.id)"
-          link>
-            <v-list-item-icon>
-              <v-icon>mdi-note</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              {{ (pageRef.id == $app.page.id) ? $app.page.data.name : pageRef.name }}
-            </v-list-item-title>
-          </v-list-item>
 
-        </template>
+      <v-list dense style="flex: 1">
+        
+        <v-tooltip right
+        :nudge-left="243"
+        :max-width="300"
+        :disabled="!$app.project.collapsedLeftSidebar"
+        v-for="pageRef in $app.page.project.pathPages" :key="pageRef.id">
 
-        <span>{{ (pageRef.id == $app.page.id) ? $app.page.data.name : pageRef.name }}</span>
+          <template v-slot:activator="{ on }">
 
-      </v-tooltip>
+            <v-list-item v-on="on"
+            :input-value="pageRef.id == $app.page.id"
+            @click="$app.project.navigateTo(pageRef.id)"
+            link>
+              <v-list-item-icon>
+                <v-icon>mdi-note</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>
+                {{ (pageRef.id == $app.page.id) ? $app.page.data.name : pageRef.name }}
+              </v-list-item-title>
+            </v-list-item>
 
-    </v-list>
+          </template>
+
+          <span>{{ (pageRef.id == $app.page.id) ? $app.page.data.name : pageRef.name }}</span>
+
+        </v-tooltip>
+
+      </v-list>
+
+    </div>
 
   </v-navigation-drawer>
   
@@ -99,5 +107,9 @@
 
 .v-list-item__icon {
   margin-right: 24px !important;
+}
+
+.v-list-item__title {
+  white-space: normal;
 }
 </style>
